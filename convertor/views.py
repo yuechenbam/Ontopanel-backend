2c59b154f1bb199550c9485cdb577b0a87b5d7a9
yuechen <qzelise@gmail.com>
2022-04-22 09:48:20 +0200

    change

--- convertor/views.py
+++ convertor/views.py
@@ -9,13 +9,6 @@ from .utilies.conversion.graph_to_rdf import MakeOntology
 from .utilies.datauploader.data_process import file_to_json
 import sys
 import traceback
-from drf_yasg.utils import swagger_auto_schema
-from drf_yasg import openapi
-from .swaggerdoc import *
-from rest_framework.parsers import (
-    FormParser,
-    MultiPartParser
-)
 
 
 class GraphConvertor(APIView):
@@ -34,19 +27,6 @@ class GraphConvertor(APIView):
 
         return result, errors
 
-    @swagger_auto_schema(
-        operation_summary='convert graph',
-        operation_description='Convert the drawio graph data into OWl.\
-                                Response data is loaded to Ontopanel-Convertor.',
-        request_body=convertor_request_schema,
-        responses={
-            "200": convertor_response,
-            "503": "Something goes wrong, please contact yue.chen@bam.de for fix this. thanks."
-        }
-
-
-
-    )
     def post(self, request):
 
         result, errors = self.process_data(request)
@@ -60,7 +40,6 @@ class GraphConvertor(APIView):
 
 class TableDataProcessor(APIView):
     permission_classes = [AllowAny]
-    parser_classes = (FormParser, MultiPartParser)
 
     def process_data(self, request):
         file_object = request.FILES.get("myfile")
@@ -79,7 +58,6 @@ class TableDataProcessor(APIView):
         except Exception as e:
             exc_type, exc_value, exc_traceback_obj = sys.exc_info()
             traceback.print_tb(exc_traceback_obj)
-            print("error", type(e))
             if type(e) == AssertionError:
                 raise APIException(
                     str(e))
@@ -89,16 +67,6 @@ class TableDataProcessor(APIView):
 
         return result
 
-    @swagger_auto_schema(
-        operation_summary='process table data ',
-        operation_description='Uploader in data mapping window that converts CSV or EXCEL to table format for data mapping.',
-        manual_parameters=tabledata_parameters,
-        responses={
-            "200": tabledata_response,
-            "503": "File type does not match or can not be processed."
-        }
-
-    )
     def post(self, request):
 
         result = self.process_data(request)
