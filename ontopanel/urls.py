f5693fdf2aba314b2f0c2b7e5c14d286468107f6
yuechen <qzelise@gmail.com>
2022-05-10 18:13:01 +0200

    a

--- ontopanel/urls.py
+++ ontopanel/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 urlpatterns = [
     path("api/v1/", include("owl_processor.urls"), name='owl_processor'),
