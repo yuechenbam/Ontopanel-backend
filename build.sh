893c0da189ffcaa6f340fe26ddde22f0938b1b25
yuechen <qzelise@gmail.com>
2024-01-11 10:08:03 +0100

    add build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ python3.9 -m pip install -r requirements.txt
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
+python3.9 manage.py createsuperuser --noinput
 
 echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
