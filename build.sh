8b77fe40b9d01c2add36acd328b0dc1ecb527263
yuechen <qzelise@gmail.com>
2024-01-11 11:20:14 +0100

    tt

--- build.sh
+++ build.sh
@@ -5,9 +5,10 @@ echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.9 manage.py createsuperuser --noinput
+
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
+python3.9 manage.py createsuperuser --noinput
 
 
 echo "Collect Static..."
